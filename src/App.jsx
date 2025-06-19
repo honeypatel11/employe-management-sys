a9b9146e419162244c7a36338719a64cd9bdc945
khushbupatel05 <khushbupatel0009@gmail.com>
2025-06-19 12:38:52 +0530

    added login successfully

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,22 @@ import About from "./pages/About"
 import Contact from "./pages/Contact"
 import Login from "./pages/Login"
 import { ToastContainer } from "react-toastify"
+import Employee from "./pages/Employee"
 
 const App = () => {
   return (
     <div>
-      <ToastContainer>
-      <BrowserRouter>
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/about" element={<About/>}></Route>
-        <Route path="/contact" element={<Contact/>}></Route>
-        <Route path="/login" element={<Login/>}></Route>
-      </Routes>
-      </BrowserRouter>
-      </ToastContainer>
+      <ToastContainer />
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/empoyee" element={<Employee/>}></Route>
+          </Routes>
+        </BrowserRouter>
     </div>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,51 +1,58 @@
 import { useState } from "react"
+import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
-const Login = () => {
-    const [input , setinput] = useState({
-        email : '', password : ''
-    });
-    const handelchange = (e) => {
-        setinput({...input, [e.target.id] : e.target.value});
-    };
-    if(input.email == "admin123@gmail.com" && input.password == "admin@123"){
-        toast.success("Logged In Successfully")
-    }else{
-         toast.alert("Logged In Not Successfully")
-    }
-    console.log(input);
-    
-  return (
- <section className="bg-gray-50 dark:bg-gray-900">
-  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-    {/* <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-      <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
-      Flowbite    
-    </a> */}
-    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-      <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-          Sign in to your account
-        </h1>
-        <form className="space-y-4 md:space-y-6" action="#">
-          <div>
-            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-            <input type="email" onChange={handelchange} name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
-          </div>
-          <div>
-            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-            <input type="password" name="password" onChange={handelchange} id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-          </div>
-          <div className="flex items-center justify-between">
-           
-            <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
-          </div>
 
-        </form>
+const Login = () => {
+  const navigate = useNavigate();
+  const [input, setinput] = useState({
+    email: '', password: ''
+  });
+  const handelchange = (e) => {
+    setinput({ ...input, [e.target.id]: e.target.value });
+  };
+  const handlesubmit = (e) => {
+    e.preventDefault();
+
+    if (input.email == "admin123@gmail.com" && input.password == "admin@123") {
+      toast.success("Log In Sucessfully");
+    } else {
+      toast.error("Log In Not Sucessfully");
+    }
+
+
+
+  }
+  console.log(input);
+
+  return (
+    <section className="bg-gray-50 dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Sign in to your account
+            </h1>
+            <form className="space-y-4 md:space-y-6" onSubmit={handlesubmit} action="#">
+              <div>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                <input type="email" onChange={handelchange} name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
+              </div>
+              <div>
+                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                <input type="password" name="password" onChange={handelchange} id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+              </div>
+              <div className="flex items-center justify-between">
+                <button type="submit"  onClick={() => navigate('/employee')} className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Sign In</button>
+
+              </div>
+
+            </form>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</section>
+    </section>
 
   )
 }
