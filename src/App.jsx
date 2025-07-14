c704dd3413d66e6b2a1fc74b9ca7addd66c2bf94
honeypatel11 <honeypatel1109@gmail.com>
2025-07-14 12:32:36 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { ToastContainer } from 'react-toastify';
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useEffect, useState } from 'react';
-import Employees from './pages/Employees';
-import ProtectedRoutes from './components/ProtectedRoutes';
 
 import AddEmployee from './pages/Addemployee';
 import Editemployee from './pages/Editemployee';
@@ -11,14 +8,17 @@ import Protectroutetwo from './components/Protectroutetwo';
 import Home from './pages/Home';
 import Header from './components/Header';
 import Contact from './pages/Contact';
+import Employee from './pages/Employee';
+import Protectroute from './components/Protectroute'; // Used for authenticated pages
+import Login from './pages/Login';
 
 const App = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     useEffect(() => {
-        let loginStatus = JSON.parse(localStorage.getItem("isLoggedIn"))
-        setIsLoggedIn(loginStatus)
-    }, [])
+        let loginStatus = JSON.parse(localStorage.getItem("isLoggedIn"));
+        setIsLoggedIn(loginStatus);
+    }, []);
 
     return (
         <BrowserRouter>
@@ -26,20 +26,35 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/Header" element={<Header />} />
-                <Route path="/services" element={<Services />} />
-                <Route path="/contact" element={<Contact/>} />
+                <Route path="/contact" element={<Contact />} />
+                
                 <Route path="/login" element={
                     <Protectroutetwo isLoggedIn={isLoggedIn}>
                         <Login setIsLoggedIn={setIsLoggedIn} />
                     </Protectroutetwo>
-                }/>
-                <Route path="/employees" element={<ProtectedRoutes Component={Employees} />} />
-                <Route path="/add-employee" element={<ProtectedRoutes Component={AddEmployee} />} />
-                <Route path="/edit-employee/:id" element={<ProtectedRoutes Component={Editemployee} />} />
+                } />
+
+                <Route path="/employees" element={
+                    <Protectroute isLoggedIn={isLoggedIn}>
+                        <Employee />
+                    </Protectroute>
+                } />
+
+                <Route path="/add-employee" element={
+                    <Protectroute isLoggedIn={isLoggedIn}>
+                        <AddEmployee />
+                    </Protectroute>
+                } />
+
+                <Route path="/edit-employee/:id" element={
+                    <Protectroute isLoggedIn={isLoggedIn}>
+                        <Editemployee />
+                    </Protectroute>
+                } />
             </Routes>
             <ToastContainer />
         </BrowserRouter>
-    )
+    );
 }
 
-export default App
+export default App;
